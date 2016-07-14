4e9ec303511074be9af50000c23ce3848c8819ea
a-gil <argil@ucsc.edu>
2016-07-14 16:23:03 -0700

    Code merged and working to get a grid of raw files

--- pre_code_function.py
+++ pre_code_function.py
@@ -10,54 +10,50 @@ import struct
 #import base64
 from sem_v3_lib import *
 #from io import BytesIO
-import msvcrt
+#import msvcrt
 #import numpy as np
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 print("So far so good.")
 
-SampleName = 'Sample Name Here'
+SampleName = 'none'
 
-ImageWidth = 500
+ImageWidth = 512
 
-ImageHeight = 500
+ImageHeight = 512
 
 NumImages = 1
 
 bpp = 16
 
-ScanSpeed = 5
+ScanSpeed = 2
 
 CaptureSE = True
 
-CaptureBSE = True
+CaptureBSE = False
+
+
+#Connect to the SEM SharkSEM interface.
+m = sem.Sem()
+conn = m.Connect('localhost', 8300)
+
 
 def ReadMessage(conn):
     # receive the message header
-
     msg_name = conn._RecvStrD(16)
-
     hdr = conn._RecvStrD(16)
-
     v = struct.unpack("<IIHHI", hdr)
-
     body_size = v[0]
 
-
     # get fn name
-
-    cb_name = DecodeString(msg_name)     
-
+    cb_name = DecodeString(msg_name)
+                   
     # receive the body
-
     cb_body = conn._RecvStrD(body_size)
 
-        
     # finished reading message
-
     return (cb_name, cb_body)
-
 
 
 def WriteImage(m):
@@ -65,175 +61,124 @@ def WriteImage(m):
     NumChannels = 0
 
     if CaptureSE == True:
-
         SEfile = open(SEFileName, "ab")
-
         NumChannels += 1
 
     if CaptureBSE == True:
-
         BSEfile = open(BSEFileName, "ab")
-
         NumChannels += 1
 
-    bytes_read = 0
 
+    bytes_read = 0
     bytesperpixel = bpp/8
+    
 
     while bytes_read < ImageWidth*ImageHeight*bytesperpixel*NumChannels:
-
         (cb_name, cb_body) = ReadMessage(m.connection)
-
         v = struct.unpack("<IiIiI", cb_body[0:20])
 
         # Channel 0, write SE image.
-
         if v[1] == 0 and CaptureSE == True:
-
             SEfile.write(cb_body[20:])
-
             bytes_read = bytes_read + v[4]
 
         # Channel 1, write BSE image.
-
         if v[1] == 1 and CaptureBSE == True:
-
             BSEfile.write(cb_body[20:])
-
             bytes_read = bytes_read + v[4]
 
         # When we are done, close the files.
-
         if bytes_read >= ImageWidth*ImageHeight*bytesperpixel*NumChannels:        
-
             if CaptureSE == True:
-
                  SEfile.close()
 
             if CaptureBSE == True:
-
                  BSEfile.close()
 
         time.sleep(1)
 
-        im = Image.frombuffer("L", bytes_read, bio, "raw", "L", 0, 1)
-
-        im.save(SEFileName +"(" + x + "," + y + ")" + ".tif")
-
-def main(x,y):
+def main(x, y):
     global SEFileName
     global BSEFileName
-
-    # Connect to the SEM SharkSEM interface.
-
-    m = sem.Sem()
-
-    conn = m.Connect('localhost', 8300)
-
+    
     if conn<0:
-
         print("Error: Unable to connect to SEM")
-
         return
-        
+
 
     ViewField = m.GetViewField()*1000 # View field in microns.
-    #Voltage = m.HVGetVoltage()/1000; # Voltage in keV.
+    Voltage = m.HVGetVoltage()/1000; # Voltage in keV.
 
 
-    SEFileName = '%s, %d keV, %dx%dx%d, %g um wide, %d bpp, little endian, SE.raw' % (SampleName, ImageWidth, ImageHeight, NumImages, ViewField, bpp)
-
-    BSEFileName = '%s, %d keV, %dx%dx%d, %g um wide, %d bpp, little endian, BSE.raw' % (SampleName, ImageWidth, ImageHeight, NumImages, ViewField, bpp)
-
+    SEFileName = '(' + str(x) + ', ' + str(y) + ') ' + '%s, %d keV, %dx%dx%d, %g um wide, %d bpp, little endian,SE.raw' % (SampleName, Voltage, ImageWidth, ImageHeight, NumImages, ViewField, bpp)
+    #BSEFileName = '%s, %d keV, %dx%dx%d, %g um wide, %d bpp, little endian, BSE.raw' % (SampleName, Voltage, ImageWidth, ImageHeight, NumImages, ViewField, bpp)
 
 
-    # Get rid of any existing files if they are there.  This is an overwrite.
-
-    if CaptureSE == True:
-        try:
-            os.remove(SEFileName)
-        except:
-            pass
-
-    if CaptureBSE == True:
-        try:
-            os.remove(BSEFileName)
-        except:
-            pass
-
-
-    # Assign SE to channel 0 and BSE to channel 1.
-
+    #Assign SE to channel 0 and BSE to channel 1.
     m.DtSelect(0, 0)
-
     m.DtSelect(1, 1)
 
-    # Enable each, 8 or 16 bits/pixel.
 
+    #Enable each, 8 or 16 bits/pixel.
     if CaptureSE == True:
-
         m.DtEnable(0, 1, bpp)
-
     else:
-
         m.DtEnable(0, 0)
-
     if CaptureBSE == True:
-
         m.DtEnable(1, 1, bpp)
-
     else:
-
         m.DtEnable(1, 0)
 
+
     # make sure scanning is inactive
-
     m.ScStopScan()
-
     m.ScSetSpeed(ScanSpeed)
 
-    # Note the stage position and apply any initial offset requested.
-
-    (x,y) = m.GetImageShift() # Gives mm.
     
-    # Note the working distance.
-
-    WD = m.GetWD() # Gives mm.
-
-    time.sleep(1)
-
-
-    # Loop through a bunch of images..
-
-    # Take an image.
-
-    res = m.ScScanXY(1, ImageWidth, ImageHeight, 0, 0, ImageWidth-1, ImageHeight-1, 1)        
-
-                #print("ScScanXY result: " + str(res))
-
-    print("Scanning image# " + str(i))
-
-    time.sleep(1)
+    #move SEM to that location
+    m.StgMoveTo(x, y)
+        
+    time.sleep(5)
+        
+    #Autofocus on that point        
+    m.AutoWD(0)
+    
+    
+    time.sleep(40)
 
 
+    for i in range(NumImages):
+        # Take an image.
+        print("Scanning image# " + str(i))
+        time.sleep(1)
 
-    # Let the image come in as it is acquired and then write it.
+        res = m.ScScanXY(1, ImageWidth, ImageHeight, 0, 0, ImageWidth-1, ImageHeight-1, 1)
+        
+        # Let the image come in as it is acquired and then write it.
+        WriteImage(m)
 
-    WriteImage(m)
-
+        print("\n\n")
+        
     print("\n\n")
-     
-    m.SetImageShift(x, y)
+
     time.sleep(1)
+
+#    if CaptureSE == True:
+#        print("\n" + str(i+1) + " images written to " + SEFileName)
+#
+#    if CaptureBSE == True:
+#        print("\n" + str(i+1) + " images written to " + BSEFileName)
 
     print('Done')
     
+    return
+
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #args will hold the arguments for the functions. This is the format of the arguments:
 #args = [x_0, y_0, x_max, x_min, y_max, y_min, delta]
 #Edit the list below to use its arguments.
-args = [5, 35]
+args = [0, 35]
 
 def calc_coords(x_0, y_0, x_max = 102, x_min = -2, y_max = 37, y_min = -65, delta = 1.8):
     """This function simply creates a rectangular area to scan over. """ \
@@ -242,28 +187,21 @@ def calc_coords(x_0, y_0, x_max = 102, x_min = -2, y_max = 37, y_min = -65, delt
     """beam to the given point, then move onto the next one based on the spacing delta."""
     
     if x_0 > x_max:
-        
         return 'Error: input x must be smaller'
         
     elif x_0 < x_min:
-        
         return 'Error: input x must be larger'
         
     elif y_0 > y_max:
-        
         return 'Error: input y must be smaller'
         
     elif y_0 < y_min:
-        
         return 'Error: input y must be larger'
     
     
     
-#    The following makes a list of 2-element tuples
-#    and they will be used as the coordinates of the beam's location
-
-########################
-########################
+#The following makes a list of 2-element tuples
+#and they will be used as the coordinates of the beam's location
     
    #dummy variable, may be removed if we make the sub while-->for    
     x_n = x_min + 1
@@ -299,14 +237,13 @@ def calc_coords(x_0, y_0, x_max = 102, x_min = -2, y_max = 37, y_min = -65, delt
     
     return coords
     
-########################
-########################
+    
 
 def TakeImgs(*args):
     """This function takes the coordinates calculated from calc_coords and feeds """\
     """them to the SEM. At each point, an image is taken. """\
     """CAUTION: please make sure to alter the max and min values if a custom stage is being used."""
-    
+
     coords = calc_coords(*args)
 
     #Now that we have the list, we can use it to assign the coordinates to the SEM
@@ -315,19 +252,17 @@ def TakeImgs(*args):
     
     
     #define a certain magical index
-    i = 0
-    
-    while i < len(coords):
+    j = 0
+    while j < len(coords):
+        x, y = coords[j]
         
-        x, y = coords[i]
+        ##move SEM to that location
+        #m.StgMoveTo(x, y)
+        #
+        ##Autofocus on that point        
+        #m.AutoWD(0)
         
-        #feed x,y to SEM function that moves stage
-        #call SEM function that will calculate the WD at this point
-        #let's say z = WD, but use a dummy function for testing
+        #call main function at coordinates
+        main(x, y)
         
-        sem.StgMoveTo(x, y)
-        #sem.AutoWD() 0 or 1 in argument
-        #main()
-        
-        #coords[i] = (x, y, round(z, 3))
-        i = i+1
+        j = j+1
