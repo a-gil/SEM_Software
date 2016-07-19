fbf91af17dca01673d76cb86d086d775851d0590
a-gil <argil@ucsc.edu>
2016-07-19 16:27:04 -0700

    Add files from SEM CPU
    image converter is here

--- img_cnvtr.py
+++ img_cnvtr.py
@@ -1,11 +1,8 @@
-from matplotlib import pyplot as plt
 import numpy as np
-from PIL import Image
-from scipy import ndimage
 from scipy import misc
 
-stuff = np.fromfile('test.raw', dtype=uint16)
+stuff = np.fromfile('check.raw', dtype=np.uint16)
 stuff.shape = (512, 512)
-plt.imshow(stuff, cmap='gray')
+#plt.imshow(stuff, cmap='gray')
 
-misc.imsave('test.tiff', stuff)
+misc.imsave('check.tiff', stuff)
